952fc922963d50dca5439eac708c2247847871ac
Ash O'Farrell <aofarrel@ucsc.edu>
2023-04-14 16:38:17 -0700

    Make python more generic

--- extract_lineage.py
+++ extract_lineage.py
@@ -2,11 +2,11 @@ import os
 from tqdm import tqdm
 import subprocess
 
-jsons = os.listdir("rand04321_results")
+jsons = os.listdir(".")
 with open("lineages.txt", "a") as lineage_file:
     for json in tqdm(jsons):
         sample = os.path.basename(json).strip("to_Ref.H37Rv.bam.results.json")
-        with subprocess.Popen(f"ack rand04321_results/{json} -o --match '(?<=sublin\": \")(.*?)(?=\")' ", 
+        with subprocess.Popen(f"ack ./{json} -o --match '(?<=sublin\": \")(.*?)(?=\")' ", 
                 shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) as ack:
             for output in ack.stdout:
                 if '"' in output.decode('UTF-8'):
