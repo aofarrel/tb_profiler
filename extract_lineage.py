a2ff085b091863edfa782f97d089fd8204796e4f
Ash O'Farrell <aofarrel@ucsc.edu>
2023-04-14 16:23:14 -0700

    Extract lineages from JSONs with python

---
+++ extract_lineage.py
@@ -0,0 +1,17 @@
+import os
+from tqdm import tqdm
+import subprocess
+
+jsons = os.listdir("rand04321_results")
+with open("lineages.txt", "a") as lineage_file:
+    for json in tqdm(jsons):
+        sample = os.path.basename(json).strip("to_Ref.H37Rv.bam.results.json")
+        with subprocess.Popen(f"ack rand04321_results/{json} -o --match '(?<=sublin\": \")(.*?)(?=\")' ", 
+                shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) as ack:
+            for output in ack.stdout:
+                if '"' in output.decode('UTF-8'):
+                    lineage_file.write(f"{sample}: ??? \n")
+                elif output.decode('UTF-8').startswith("\n"):
+                    pass
+                else:
+                    lineage_file.write(f"{sample}: {output.decode('UTF-8')}")
